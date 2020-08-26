5b4f52060e3495d2df5bf44ce3ae8c989afd8f5c
Merojiddin <merojiddin_1998@mail.ru>
2020-08-27 00:51:15 +0800

    Updates

---
+++ precache-manifest.adcba1e5ff42da38ad23b4799a60cad1.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de3a858f462ab079305c3410074266a",
+    "url": "/Tantora/index.html"
+  },
+  {
+    "revision": "39d11f3a02cc2b0f1af8",
+    "url": "/Tantora/static/css/10.525a09b5.chunk.css"
+  },
+  {
+    "revision": "e61d3d3b5fa590fcbec1",
+    "url": "/Tantora/static/css/3.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b9cd59a67913a7247cab",
+    "url": "/Tantora/static/css/main.3ea9ca9d.chunk.css"
+  },
+  {
+    "revision": "488460bf907cf194fe00",
+    "url": "/Tantora/static/js/0.25872f3f.chunk.js"
+  },
+  {
+    "revision": "39d11f3a02cc2b0f1af8",
+    "url": "/Tantora/static/js/10.3a3e7120.chunk.js"
+  },
+  {
+    "revision": "a7d7f24b3723de7ed142",
+    "url": "/Tantora/static/js/11.5a0dc735.chunk.js"
+  },
+  {
+    "revision": "da86e2375b39ccf61ca8",
+    "url": "/Tantora/static/js/12.e4db8b55.chunk.js"
+  },
+  {
+    "revision": "f30dc9e0c9d81c5dec54",
+    "url": "/Tantora/static/js/13.b5c0e34e.chunk.js"
+  },
+  {
+    "revision": "e61d3d3b5fa590fcbec1",
+    "url": "/Tantora/static/js/3.b1959dbb.chunk.js"
+  },
+  {
+    "revision": "03140b8674c322b944f27d8b5f641e68",
+    "url": "/Tantora/static/js/3.b1959dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7259cad712827de4715",
+    "url": "/Tantora/static/js/4.0eac7698.chunk.js"
+  },
+  {
+    "revision": "146edc3b1c808ceed819",
+    "url": "/Tantora/static/js/5.11240327.chunk.js"
+  },
+  {
+    "revision": "a9cb338d37b2c244aa44",
+    "url": "/Tantora/static/js/6.1adc2b1f.chunk.js"
+  },
+  {
+    "revision": "e1051993372cddf12ba8",
+    "url": "/Tantora/static/js/7.da2b8862.chunk.js"
+  },
+  {
+    "revision": "abfedccc2a42041400bd",
+    "url": "/Tantora/static/js/8.09894e63.chunk.js"
+  },
+  {
+    "revision": "a1c7155387e436637f4e",
+    "url": "/Tantora/static/js/9.4c6cf8c5.chunk.js"
+  },
+  {
+    "revision": "b9cd59a67913a7247cab",
+    "url": "/Tantora/static/js/main.f655436e.chunk.js"
+  },
+  {
+    "revision": "1ec9816f97bdff4bff3c",
+    "url": "/Tantora/static/js/runtime-main.7af2241f.js"
+  },
+  {
+    "revision": "4b7ad49ac3bd58ca3b47eda3450b2826",
+    "url": "/Tantora/static/media/Exhibitors.4b7ad49a.png"
+  },
+  {
+    "revision": "4f9a153f3af026fc9f4fffb3ba9e873e",
+    "url": "/Tantora/static/media/Organizators.4f9a153f.png"
+  },
+  {
+    "revision": "4b4af20f3f9420712693fd3a178c14d8",
+    "url": "/Tantora/static/media/about.4b4af20f.jpg"
+  },
+  {
+    "revision": "23f888b50d2bafdf3ecd8c939fc974e5",
+    "url": "/Tantora/static/media/bgImg.23f888b5.jpg"
+  },
+  {
+    "revision": "f9028a20ffc8f08e6b80e294a111fae4",
+    "url": "/Tantora/static/media/homeEventBg.f9028a20.jpg"
+  },
+  {
+    "revision": "3daa1afacde4bd7435170aadc23f30cf",
+    "url": "/Tantora/static/media/icon.3daa1afa.png"
+  },
+  {
+    "revision": "ed5c19d798486377dcec98bce3958d80",
+    "url": "/Tantora/static/media/ipad.ed5c19d7.png"
+  },
+  {
+    "revision": "c914071bee7d5b71664b6fa72ab9f7cd",
+    "url": "/Tantora/static/media/login-bg.c914071b.jpg"
+  },
+  {
+    "revision": "4357088379617f66cae5528085bdf159",
+    "url": "/Tantora/static/media/mainBg.43570883.jpg"
+  },
+  {
+    "revision": "f869b7bb1b08c8776243c2faafe8ca1a",
+    "url": "/Tantora/static/media/mainBg.f869b7bb.jpg"
+  },
+  {
+    "revision": "9aa4d41ef3af0ebbde5bba349ab181ef",
+    "url": "/Tantora/static/media/mainbackg.9aa4d41e.jpg"
+  },
+  {
+    "revision": "e9edb89f89227c6c2a6989e08b19f3a0",
+    "url": "/Tantora/static/media/mob.e9edb89f.png"
+  },
+  {
+    "revision": "1a46a36532e82d6062b7f39a22432899",
+    "url": "/Tantora/static/media/register-bg.1a46a365.jpg"
+  },
+  {
+    "revision": "1e753b01772f4e8e7484fc38b9cee988",
+    "url": "/Tantora/static/media/testimolbg.1e753b01.png"
+  },
+  {
+    "revision": "e853cd3c1bd203e40bf054c6273476e2",
+    "url": "/Tantora/static/media/video.e853cd3c.png"
+  }
+]);
